cd35ffa818f9b6643c8cbf861127e7394e3d7746
Jude <jude@judemelancon.net>
2024-06-29 20:25:25 -0500

    Improve display of solved groups

--- src/components/PuzzleGrid.tsx
+++ src/components/PuzzleGrid.tsx
@@ -3,70 +3,65 @@
 import { useCallback, useEffect, useState } from "react";
 import classNames from "classnames";
 import styles from "./PuzzleGrid.module.css";
-import { Puzzle, ColorClassNames, Item } from "@/types";
+import { Puzzle, IndexedGroup, Item } from "@/types";
 import shuffle from "@/util/shuffle";
 
 const InitialStrikes = 4;
 
 export default function PuzzleGrid({ puzzle }: Readonly<{ puzzle: Puzzle }>) {
-    const [items, setItems] = useState<Item[]>(extractItems(puzzle));
-    const [solvedConnections, setSolvedConnections] = useState<string[]>([]);
+    const [unsolvedItems, setUnsolvedItems] = useState<Item[]>(extractItems(puzzle));
+    const [solvedGroups, setSolvedGroups] = useState<IndexedGroup[]>([]);
     const [strikes, setStrikes] = useState(InitialStrikes);
 
     useEffect(() => {
         // Shuffling at initialization time produces hydration errors.
-        const shuffled = [...items];
-        shuffle(shuffled);
-        setItems(shuffled);
+        setUnsolvedItems(previous => shuffle([...previous]));
     }, []);
 
     useEffect(() => {
-        const activeItems = items.filter(item => item.active);
+        const activeItems = unsolvedItems.filter(item => item.active);
         if (activeItems.length === 4) {
-            const matchingGroup = puzzle.groups.find(group => activeItems.every(item => group.items.includes(item.text)));
-            if (matchingGroup) {
-                const alreadyDone = items.filter(item => item.done);
-                activeItems.sort((a, b) => a.text < b.text ? -1 : a.text > b.text ? 1 : 0);
-                for (const item of activeItems) {
-                    item.done = true;
-                    item.active = false;
-                }
-                const remainder = items.filter(item => !item.done);
-                setItems([...alreadyDone, ...activeItems, ...remainder]);
-                setSolvedConnections(previous => [...previous, matchingGroup.connection]);
+            const matchingGroupIndex = puzzle.groups.findIndex(group => activeItems.every(item => group.items.includes(item.text)));
+            if (matchingGroupIndex >= 0) {
+                setUnsolvedItems(previous => previous.filter(item => !item.active));
+                setSolvedGroups(previous => [...previous, { ...puzzle.groups[matchingGroupIndex], index: matchingGroupIndex }]);
             }
             else {
-                setStrikes(previous => previous - 1);
                 for (const item of activeItems)
                     item.active = false;
-                setItems([...items]);
+                setUnsolvedItems(previous => [...previous]);
+                setStrikes(previous => previous - 1);
             }
         }
-    }, [items, puzzle.groups]);
+    }, [unsolvedItems, puzzle.groups]);
 
     const handleItemClick = useCallback(
         (target: Item) => {
-            setItems(previous => {
+            setUnsolvedItems(previous => {
                 const i = previous.findIndex(item => item.text === target.text);
                 return previous.with(i, { ...previous[i], active: !target.active });
             });
         },
         []);
 
-    const itemElements = items?.map(item => (
-        <button
-            key={item.text}
-            className={classNames(styles.item, item.active && styles.active, item.done ? styles[item.color] : strikes && styles.todo)}
-            onClick={item.done || !strikes ? undefined : e => handleItemClick(item)}>
-            {item.text}
-        </button>
-    ));
-
     return (<>
         <div className={classNames(styles.grid, styles.items)}>
-            {itemElements}
+            {solvedGroups.map(group => (
+                <div key={group.connection} className={classNames(styles.group, styles[`group-${group.index}`])}>
+                    <span className={styles.connection}>{group.connection}</span>
+                    <span className={styles.solvedItems}>{group.items.join(', ')}</span>
+                </div>
+            ))}
+            {unsolvedItems.map(item => (
+                <button
+                    key={item.text}
+                    className={classNames(styles.item, item.active && styles.active, strikes && styles.todo)}
+                    disabled={!strikes}
+                    onClick={!strikes ? undefined : e => handleItemClick(item)}>
+                    {item.text}
+                </button>
+            ))}
         </div>
-        {/* TODO {solvedConnections?.map(connection => <div key={connection} className={styles.connection}>{connection}</div>)} */}
         <div className={styles.strikes}>
             <span>{[...Array(InitialStrikes)].map((_, i) => i >= strikes ? '😞' : '😏')}</span>
         </div>
@@ -77,13 +72,9 @@ function extractItems(puzzle: Puzzle): Item[] {
     return (
         puzzle
             .groups
-            .flatMap((group, i) =>
+            .flatMap(group =>
                 group
                     .items
-                    .map(itemText => ({
-                        text: itemText,
-                        color: ColorClassNames[i],
-                        done: false,
-                        active: false
-                    }))));
+                    .map(text => ({ text, active: false })))
+    );
 }
--- src/types.ts
+++ src/types.ts
@@ -3,19 +3,17 @@ export type Group = {
     items: string[];
 };
 
+export type IndexedGroup = Group & {
+    index: number;
+};
+
 export type Puzzle = {
     slug: string;
     name: string;
     groups: Group[];
 };
 
-export type ColorClassName = 'blue' | 'red' | 'yellow' | 'grey';
-
-export const ColorClassNames: ColorClassName[] = ['blue', 'red', 'yellow', 'grey'];
-
 export type Item = {
     text: string;
-    color: ColorClassName;
-    done: boolean;
     active: boolean;
 };
